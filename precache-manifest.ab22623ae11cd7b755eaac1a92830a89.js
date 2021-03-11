e1af02d59e7ea2f3f6244fe8ab1fc473ce31b26c
Akopova-web <aakopova44@gmail.com>
2021-03-11 17:28:12 +0300

    Updates

---
+++ precache-manifest.ab22623ae11cd7b755eaac1a92830a89.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f47ec9de7a47106c54498fa1d844b28",
+    "url": "/demo-network-for-students/index.html"
+  },
+  {
+    "revision": "6dba0d6ea5ed2041486f",
+    "url": "/demo-network-for-students/static/css/2.61833881.chunk.css"
+  },
+  {
+    "revision": "d0d6b279fae24857ea4b",
+    "url": "/demo-network-for-students/static/css/main.06095c8f.chunk.css"
+  },
+  {
+    "revision": "6dba0d6ea5ed2041486f",
+    "url": "/demo-network-for-students/static/js/2.c9b10fe2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/demo-network-for-students/static/js/2.c9b10fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d6b279fae24857ea4b",
+    "url": "/demo-network-for-students/static/js/main.d01a2446.chunk.js"
+  },
+  {
+    "revision": "06d0aa1a7d82e3e5cd71",
+    "url": "/demo-network-for-students/static/js/runtime-main.e4681f09.js"
+  },
+  {
+    "revision": "c7b5f6a007a193933d22f1b03bf2b43e",
+    "url": "/demo-network-for-students/static/media/empty_avatar.c7b5f6a0.png"
+  },
+  {
+    "revision": "e8c852d1c749e33aa27a89898ff3a7d6",
+    "url": "/demo-network-for-students/static/media/loading.e8c852d1.svg"
+  },
+  {
+    "revision": "fa112755b15ea2aa0d19b46cabc0d572",
+    "url": "/demo-network-for-students/static/media/logo2.fa112755.png"
+  },
+  {
+    "revision": "458f97fe106a8fdc5575f23cb44ed477",
+    "url": "/demo-network-for-students/static/media/Шапка.458f97fe.png"
+  }
+]);
